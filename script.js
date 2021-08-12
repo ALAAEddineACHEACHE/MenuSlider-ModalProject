121683b849cc2098ded4506cdbbdcdf243739d51
ALAE <57995821+ALAAEddineACHEACHE@users.noreply.github.com>
2021-08-12 21:01:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 let nav = document.getElementsByTagName("nav");
 let boutton = document.getElementById("toggle");
 let body = document.getElementById("body");
-boutton.addEventListener("click",function(event)    {
+boutton.addEventListener("click",function(event){
     event.preventDefault();
-    console.log(body);
     body.classList.toggle("toggles");
-   
-    
-    
 
 });
